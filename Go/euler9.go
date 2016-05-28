35c165b9cd75adb26059771f07317f690a51565c
Mertcan Ekiz <ekiz.mertcan@gmail.com>
2016-05-28 19:42:21 +0300

    Problem 9

---
+++ Go/euler9.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func euler() {
+	target := 1000
+	a_limit := target / 3
+	b_limit := target / 2
+	for a := 1; a < a_limit; a++ {
+		for b := a; b < b_limit; b++ {
+			c := 1000 - b - a
+			if a*a+b*b == c*c {
+				fmt.Println(a * b * c)
+				return
+			}
+		}
+	}
+}
+
+func main() {
+	t1 := time.Now()
+	euler()
+	fmt.Println("Elapsed time:", time.Since(t1))
+}
