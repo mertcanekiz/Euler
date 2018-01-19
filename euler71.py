ca62bd0bec247db7a6bb1e8939d197254e2eb7c9
Mertcan Ekiz <ekiz.mertcan@gmail.com>
2018-01-19 23:01:52 +0300

    Problem 71

---
+++ euler71.py
@@ -0,0 +1,24 @@
+from euler import timer
+from sortedcontainers import SortedSet
+from math import gcd, floor, ceil
+from fractions import Fraction
+
+
+def euler():
+    fracs = SortedSet()
+    limit = 10**6
+    for d in range(5, limit+1):
+        target = d*3/7
+        for n in range(int(floor(target)), int(ceil(target))+1):
+            if gcd(n, d) == 1:
+                fracs.add(Fraction(n, d))
+    result = 0
+    compare = Fraction(3, 7)
+    for i, f in enumerate(fracs):
+        if f == compare:
+            result = i-1
+            break
+    print(fracs[result])
+
+
+euler()
